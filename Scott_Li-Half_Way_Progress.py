29ab3cb057123539baf5beb2a0a4b12ef45c4e9d
glace23 <vikash23galce@gmail.com>
2020-12-17 00:21:45 -0800

    update comments

--- Scott_Li-Half_Way_Progress.py
+++ Scott_Li-Half_Way_Progress.py
@@ -11,16 +11,25 @@
 class FocusGame:
     """
     Creates a Focus Game object.
-        References Player and Board class
+    Functions with attributes related to the game.
+    Communicates with player class to get and set player attributes.
+    Communicates with board class to get board.
     """
 
     def __init__(self, player1, player2):
-        """Initiate variables that help track the game."""
+        """
+        Initiate variables that help track the game.
+        _turn keeps track of which turn
+        _odd_turn_player helps keep track which player is playing odd turn
+        _even_turn_player same as _odd_turn_player but even turn
+        _total_set_piece keeps track how many pieces of one color there is
+        _stack_height keeps track the allowed height to be stacked
+        """
         # initialize attributes
         self._turn = 1
         self._odd_turn_player = None
         self._even_turn_player = None
-        self._set_piece_to_win = 6
+        self._total_set_piece = 18
         self._stack_height = 5
 
         # initialize players
@@ -32,17 +41,25 @@ class FocusGame:
         self._gameboard = self._board.get_board()
 
     def move_piece(self, name, origin, destination, pieces):
-        """
-            Moves pieces from origin to destination on game board.
+        """ Moves pieces from origin to destination on game board.
 
             :Keyword arguments:
             name -- player name
             origin -- original coordinates
             destination -- destination coordinates
             pieces -- number of pieces to move
+
+
+            _check_move_piece_legal checks various flags whether the move is permitted or not.
+            _move_to_list moves the last item(s) depending on the pieces moved to the destination stack.
+            _check_take_piece checks the dequeued piece to go to reserve or captured
+            returns the winner when _check_win returns True
+            returns 'sucessfully moved' when the piece is moved
+            else returns the msg of specific flag
         """
         # check if there is any illegal commands
         legality = self._check_move_piece_legal(name, origin, destination, pieces)
+        piece_list = []
 
         if legality is True:
             # add turn if pass checks
@@ -75,20 +92,26 @@ class FocusGame:
         return len(self._get_player_by_name(name).get_captured())
 
     def reserved_move(self, name, position):
-        """Move reserve piece to board."""
+        """
+        Move reserve piece to board.
+        _check_move_reserve_legal checks various flags to make sure move is permitted
+        if no reserve for player, returns no pieces in reserve
+        _move_to_list moves the last item(s) depending on the pieces moved to the destination stack.
+        _check_take_piece checks the dequeued piece to go to reserve or captured
+        returns the winner when _check_win returns True
+        else returns the msg of specific flag
+        """
         legality = self._check_move_reserve_legal(name, position)
         if legality is True:
             # check if reserve
             if self.show_reserve(name) == 0:
-                # 'no pieces in reserve'
-                return False
+                return 'no pieces in reserve'
 
             # move turn
             self._turn += 1
 
             # move reserve to position
             self._move_to_list(self._get_player_by_name(name).get_reserve(), self._gameboard[position], 1)
-
             # check if position list length and take piece if necessary
             self._check_take_piece(name, position)
 
@@ -100,12 +123,19 @@ class FocusGame:
         else:
             return legality
 
+    def show_board(self):
+        """Prints game board."""
+        self._gameboard.show_board()
+
     def get_board(self):
         """Return game board."""
-        return self._board
+        return self._gameboard
 
-    def show_turn(self):
-        """Show whose turn is it"""
+    def show_turn(self, int=0):
+        """
+        Show whose turn is it.
+        returns and prints the which player's turn it is.
+        """
         # return player
         if self._turn % 2 == 1:
             print(f"It is turn {self._turn} and {self._odd_turn_player}'s turn")
@@ -114,33 +144,37 @@ class FocusGame:
             print(f"It is turn {self._turn} and {self._even_turn_player}'s turn")
             return self._even_turn_player
 
-    def get_set_piece_to_win(self):
-        """Returns pieces required to capture to win."""
-        return self._set_piece_to_win
-
-    def set_set_piece_to_win(self, num):
-        """Sets pieces required to capture to win."""
-        self._set_piece_to_win = num
-
     # --------Helper functions--------#
 
     def _check_player(self, name):
-        """Check if player is initialized"""
+        """
+        Check if player is initialized
+        :returns true when player name is initialized
+        else false
+        """
         if name == self._p1.get_name() or name == self._p2.get_name():
             return True
         return False
 
-    def _check_move(self, origin, destination, pieces):
-        """Check move is legal or not."""
-        # |(X1 - X2)| + |(Y1 - Y2)| <= pieces is legal
-        # move distance <= pieces moved
+    def _check_move(self, origin, destination):
+        """
+        Check move is legal or not.
+        :returns true if the piece is only moved 1 unit either horizontally or vertically
+        else false
+        """
+        # |(X1 - X2)| + |(Y1 - Y2)| = 1 is legal
         moved = abs(origin[0] - destination[0]) + abs(origin[1] - destination[1])
-        if moved == pieces:
+        if moved == 1:
             return True
         return False
 
     def _check_turn(self, name):
-        """Check if is player's turn"""
+        """
+        Check if is player's turn
+        sets the player turn order based on who moved in turn 1, anyone can move first
+        :returns true when the player who is trying to move matches the player who is supposed to move
+        on that turn
+        """
         if self._turn == 1:
             self._odd_turn_player = name
             # set even player name to other player name
@@ -156,7 +190,13 @@ class FocusGame:
         return False
 
     def _check_pieces(self, name, origin, pieces):
-        """Check if pieces are legal"""
+        """
+        Check if pieces are legal
+        :returns false when trying to move 0 pieces or piece number larger than the stack number
+        returns true when trying to move piece number less than consecutive same color piece number.
+        E.g. START 'R','G','R','R','R' END  pieces = 3 is True, pieces = 4 is False
+        returns false if no sufficient pieces to move
+        """
         # check if moved pieces smaller than 1 or larger than total piece on origin
         stack = self._gameboard[origin]
         if 1 > pieces or pieces > len(stack):
@@ -164,9 +204,9 @@ class FocusGame:
 
         colour = self._get_player_by_name(name).get_color()
         count = 0
-        # check if enough consecutive player piece in origin stack
+        # check if enough player piece in origin stack
         for i in range(0, len(stack)):
-            # if last item in list is player color, or the item on top of stack
+            # if last item in list is player color
             if stack[-1 - i] == colour:
                 count += 1
                 # if enough pieces to move in list
@@ -176,49 +216,41 @@ class FocusGame:
                 return False
 
     def _check_position(self, position):
-        """Check if position exists."""
+        """
+        Check if position exists.
+        returns true when the position is not out of bounds,
+        else false
+        """
         if self._gameboard.get(position) is not None:
             return True
         return False
 
     def _check_move_piece_legal(self, name, origin, destination, pieces):
-        """Check legality of different conditions."""
+        """
+        Check legality of different conditions.
+        returns different statements depending on the flag violated first
+        """
         if not self._check_player(name):
-            # 'player does not exist'
-            return False
-
+            return 'player does not exist'
         if not self._check_turn(name):
-            # 'not your turn'
-            return False
-
-        # if source or destination locations are invalid
-        if not self._check_position(origin) or not self._check_position(destination):
-            # 'invalid location'
-            return False
-
-        # if move length != pieces
-        if not self._check_move(origin, destination, pieces):
-            # 'invalid location'
-            return False
-
+            return 'not your turn'
+        if not self._check_move(origin, destination):
+            return 'invalid location'
         if not self._check_pieces(name, origin, pieces):
-            # 'invalid number of pieces'
-            return False
+            return 'invalid number of pieces'
         return True
 
     def _check_move_reserve_legal(self, name, position):
-        """Check legality of different conditions."""
+        """
+        Check legality of different conditions.
+        returns different statements depending on the flag violated first
+        """
         if not self._check_player(name):
-            # 'player does not exist'
-            return False
-
+            return 'player does not exist'
         if not self._check_turn(name):
-            # 'not your turn'
-            return False
-
+            return 'not your turn'
         if not self._check_position(position):
-            # 'invalid location'
-            return False
+            return 'invalid location'
         return True
 
     def _move_to_list(self, origin_list, destination_list, max_range):
@@ -228,6 +260,8 @@ class FocusGame:
             origin_list -- list to move from
             destination_list -- list to move to
             max_range -- number of times to move items
+
+            returns the modified list
         """
         for i in range(0, max_range):
             # pop last item in list
@@ -236,26 +270,35 @@ class FocusGame:
         return destination_list
 
     def _check_take_piece(self, name, origin):
-        """Checks to take piece or not."""
-        # if stack is taller than allowed stack height
+        """
+        Checks to take piece or not.
+        checks whether the coordinates of parameter origin has a list larger than the allowed stack height which is 5
+        if larger, than remove items equal to the difference of two length
+        dequeue items from the origin list
+        then go check where the pieces should be put in.
+        """
         if len(self._gameboard[origin]) > self._stack_height:
             for k in range(self._stack_height, len(self._gameboard[origin])):
-                # remove first piece from list
-                a_piece = self._gameboard[origin].pop(0)
+                # remove first item in list
+                item = self._gameboard[origin].pop(0)
                 # move to player reserve or captured
-                self._take_piece(name, a_piece)
+                self._take_piece(name, item)
         return False
 
-    def _take_piece(self, name, a_piece):
-        """Move piece to reserve or captured."""
+    def _take_piece(self, name, item):
+        """
+        Move piece to reserve or captured.
+        if color is same as player, move piece to reserve
+        if color is not same as player, move piece to captured
+        """
         # check player's color
         p = self._get_player_by_name(name)
-        # if same color as removed piece, move to reserve
-        if p.get_color() == a_piece:
-            p.set_reserve(a_piece)
-        # if different color as removed piece, move to reserve
-        elif p.get_color() != a_piece:
-            p.set_captured(a_piece)
+        if p.get_color() == item:
+            # if same color, move to reserve
+            p.set_reserve(item)
+        elif p.get_color() != item:
+            # if different color, move to captured
+            p.set_captured(item)
 
     def _get_player_by_name(self, name):
         """Return player object."""
@@ -265,38 +308,25 @@ class FocusGame:
             return self._p2
 
     def _check_win(self, name):
-        """Check if player wins the game."""
+        """
+        Check if player wins the game.
+        if total captured piece from one player equals to the total set piece, which is total piece/2
+        Return true if equal, the player has won the game due to no more pieces from opponent.
+        else false
+        """
         p = self._get_player_by_name(name)
-        # if total captured = total piece to capture to win, player wins
-        if len(p.get_captured()) == self._set_piece_to_win:
+        # if total captured = total set piece, player wins
+        if len(p.get_captured()) == self._total_set_piece:
             return True
         return False
 
-    # def _check_win(self, name):
-    #     """Check if player wins the game."""
-    #     opponent = None
-    #     # determine opponent name
-    #     if name == self._odd_turn_player:
-    #         opponent = self._even_turn_player
-    #     elif name == self._even_turn_player:
-    #         opponent = self._odd_turn_player
-    #
-    #     # player wins by opponent not having movable pieces
-    #     for key in self._gameboard:
-    #         # check if coordinate has piece
-    #         if len(self._gameboard.get(key)) > 0:
-    #             # check if player has all top stack
-    #             if self._gameboard.get(key)[-1] != self._get_player_by_name(name).get_color():
-    #                 return False
-    #     # check if opponent has any reserve after looping through all coordinates
-    #     if self.show_reserve(opponent) == 0:
-    #         return True
-    #     else:
-    #         return False
-
 
 class Player:
-    """Creates player object."""
+    """
+    Creates player object.
+    Functions with attributes related to the player object.
+    """
+
     def __init__(self, player):
         # initialize players
         self._name = player[0]
@@ -338,7 +368,12 @@ class Player:
 
 
 class Board:
-    """Creates a board object."""
+    """
+    Creates a board object.
+    Functions with attributes related to the board object.
+    Communicates with player class to set board to each player's color
+    """
+
     def __init__(self, player1, player2):
         """Initialize board and board pieces."""
         # initialize player attributes
@@ -347,7 +382,6 @@ class Board:
         self._p1_color = self._p1.get_color()
         self._p2_color = self._p2.get_color()
 
-        # sets game board by player color
         self._board = {
             (0, 0): [self._p1_color], (0, 1): [self._p1_color], (0, 2): [self._p2_color],
             (0, 3): [self._p2_color], (0, 4): [self._p1_color], (0, 5): [self._p1_color],
@@ -372,38 +406,6 @@ class Board:
         for i in range(0, 6):
             row = ''
             for j in range(0, 6):
-                row = row + f'{(i,j)}:{self._board[(i,j)]} '
+                row = row + f'{(i, j)}:{self._board[(i, j)]} '
             print(row)
 
-    def get_coordinates(self, coordinates):
-        """Returns the list in board coordinate"""
-        return self._board[coordinates]
-
-
-def main():
-    game = FocusGame(('PlayerA', 'R'), ('PlayerB', 'G'))
-    print(game.move_piece('PlayerA', (0, 0), (0, 1), 1))  # Returns message "successfully moved"
-    print(game.show_pieces((0, 1)))  # Returns ['R','R']
-    print(game.move_piece('PlayerB', (0, 2), (0, 3), 1))
-    print(game.show_pieces((0, 3)))  # Returns ['R','R']
-    print(game.move_piece('PlayerA', (0, 1), (1, 2), 2))  # Returns message "successfully moved"
-    print(game.show_pieces((1, 2)))
-    print(game.reserved_move('PlayerB', (0, 0)))  # Returns message "No pieces in reserve"
-    print(game.move_piece('PlayerB', (5, 5), (5, 4), 1))
-    print(game.move_piece('PlayerA', (1, 2), (0, 4), 3))  # Returns message "successfully moved"
-    print(game.move_piece('PlayerB', (5, 4), (5, 2), 2))
-    print(game.get_board().show_board())
-    print(game.move_piece('PlayerA', (0, 4), (4, 4), 4))  # Returns message "successfully moved"
-    print(game.move_piece('PlayerB', (5, 2), (5, 4), 2))
-    print(game.move_piece('PlayerA', (4, 4), (0, 5), 5))  # Returns message "successfully moved"
-    print(game.show_pieces((0, 5)))
-    print(game.get_board().show_board())
-    print(game.show_reserve('PlayerA'))
-    print(game.move_piece('PlayerB', (5, 4), (5, 2), 2))
-    print(game.move_piece('PlayerA', (0, 5), (2, 2), 5))  # Returns message "successfully moved"
-    print(game.show_pieces((2, 2)))
-    print(game.get_board().show_board())
-    print(game.show_captured('PlayerA'))
-
-if __name__ == '__main__':
-    main()
